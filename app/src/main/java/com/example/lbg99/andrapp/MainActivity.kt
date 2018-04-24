a187c544243d4787888cfc303f3f93ecbb69120c
Kupriyanov Alexandr <kupriyanov@alumni.tsu.ru>
2018-04-24 17:33:45 +0700

    Add_Buttons

--- app/src/main/java/com/example/lbg99/andrapp/MainActivity.kt
+++ app/src/main/java/com/example/lbg99/andrapp/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.lbg99.andrapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun testtt(view: View)
+    {
+        val Testt=Toast.makeText(this,"PLS, HELP!!!", Toast.LENGTH_SHORT)
+        Testt.show()
+    }
+    fun fix(view: View)
+    {
+        val str = textView.text.toString()
+        val cnt = Random()
+        textView.text = cnt.nextInt().toString()
     }
 }
