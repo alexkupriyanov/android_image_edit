6a5009ce5bffa39a25f16b87e2d52281031425dc
DESKTOP-7E5IHDF\lbg99 <lbglbg9992gmail.com>
2018-04-28 11:14:17 +0700

    dfgbhnfg

--- app/src/main/java/com/example/lbg99/andrapp/MainActivity.kt
+++ app/src/main/java/com/example/lbg99/andrapp/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.example.lbg99.andrapp
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import android.provider.MediaStore
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
-
+    val CAMERA_REQUEST_CODE = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cameraBtn.setOnClickListener {
+            val callCamerIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if(callCamerIntent.resolveActivity(packageManager) != null){
+                startActivityForResult(callCamerIntent,CAMERA_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if(resultCode == Activity.RESULT_OK && data != null) {
+                    photoImageView.setImageBitmap(data.extras.get("data") as Bitmap)
+                }
+            }
+            else ->  {
+                Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
