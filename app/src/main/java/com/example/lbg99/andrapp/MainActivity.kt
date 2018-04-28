a700cd39c112f34439e95968ada937aa47830335
Kupriyanov Alexandr <kupriyanov@alumni.tsu.ru>
2018-04-28 12:42:40 +0700

    Add file request

--- app/src/main/java/com/example/lbg99/andrapp/MainActivity.kt
+++ app/src/main/java/com/example/lbg99/andrapp/MainActivity.kt
@@ -4,29 +4,37 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat.startActivityForResult
+import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import com.example.lbg99.andrapp.R.id.photoImageView
 import kotlinx.android.synthetic.main.activity_main.*
-import android.R.attr.bitmap
-import android.annotation.TargetApi
-import android.os.Build
-import android.support.annotation.RequiresApi
+import java.io.File
 import java.io.IOException
+import android.os.Environment.getExternalStorageDirectory
+import android.util.Log
+import java.net.URI
 
 
 class MainActivity : AppCompatActivity() {
     private val CAMERA_REQUEST_CODE = 0
     private val REQUEST_SELECT_IMAGE_IN_ALBUM = 1
+    private var mImageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         cameraBtn.setOnClickListener {
             val callCamerIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            if(callCamerIntent.resolveActivity(packageManager) != null) {
-                startActivityForResult(callCamerIntent,CAMERA_REQUEST_CODE)
-            }
+            val photo: File
+            photo = this.createTemporaryFile("pic",".jpg")
+            photo.delete()
+            mImageUri = Uri.fromFile(photo);
+            callCamerIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+            startActivityForResult(callCamerIntent, CAMERA_REQUEST_CODE);
         }
         galeryBtn.setOnClickListener {
             val callGalleryIntent = Intent(Intent.ACTION_GET_CONTENT)
@@ -37,6 +45,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun createTemporaryFile(part: String, ext: String): File {
+        var tempDir = Environment.getExternalStorageDirectory()
+        tempDir = File(tempDir.absolutePath + "/.temp/")
+        if (!tempDir.exists()) {
+            tempDir.mkdirs()
+        }
+        return File.createTempFile(part, ext, tempDir)
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -44,17 +60,14 @@ class MainActivity : AppCompatActivity() {
         when(requestCode) {
             CAMERA_REQUEST_CODE -> {
                 if(resultCode == Activity.RESULT_OK && data != null) {
-                    photoImageView.setImageBitmap(data.extras.get("data") as Bitmap)
+                    bitmap = MediaStore.Images.Media.getBitmap(contentResolver, mImageUri)
+                    photoImageView.setImageBitmap(bitmap)
                 }
             }
             REQUEST_SELECT_IMAGE_IN_ALBUM -> {
                 if (resultCode === Activity.RESULT_OK) {
                     val selectedImage = data?.getData()
-                    try {
-                        bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedImage)
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
+                    bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedImage)
                     photoImageView.setImageBitmap(bitmap)
                 }
             }
