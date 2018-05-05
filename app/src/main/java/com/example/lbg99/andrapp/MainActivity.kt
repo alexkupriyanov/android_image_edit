3684d3c361d89a842edac2ee4071a9dd3a7a3baf
Kupriyanov Alexandr <kupriyanov@alumni.tsu.ru>
2018-05-05 10:24:35 +0700

    Merge "Kupriyanov" and "Test"

--- app/src/main/java/com/example/lbg99/andrapp/MainActivity.kt
+++ app/src/main/java/com/example/lbg99/andrapp/MainActivity.kt
@@ -1,40 +1,28 @@
 package com.example.lbg99.andrapp
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.net.Uri
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.provider.MediaStore
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
-import android.R.attr.bitmap
-import android.annotation.SuppressLint
-import android.annotation.TargetApi
 import android.graphics.BitmapFactory
-import android.os.Build
+import android.graphics.drawable.BitmapDrawable
+import android.os.Bundle
 import android.os.Environment
-import android.support.annotation.RequiresApi
-import java.io.File
-import java.io.IOException
-import android.os.Environment.DIRECTORY_PICTURES
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat.startActivity
 import android.support.v4.content.FileProvider
-import android.opengl.ETC1.getHeight
-import android.opengl.ETC1.getWidth
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import com.example.lbg99.andrapp.R.id.cameraBtn
-import com.example.lbg99.andrapp.R.id.galeryBtn
-import com.example.lbg99.andrapp.R.id.filtBtn
-import com.example.lbg99.andrapp.R.attr.logo
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.example.lbg99.andrapp.R.id.photoImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
-import android.graphics.drawable.BitmapDrawable
-
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -72,7 +60,7 @@ class MainActivity : AppCompatActivity() {
                     e.printStackTrace()
                 }
                 if (photoFile != null) {
-                    val auth: String  = packageName + ".fileprovider"
+                    val auth: String = packageName + ".fileprovider"
                     val photoURI = FileProvider.getUriForFile(this,
                             auth,
                             photoFile)
@@ -84,35 +72,38 @@ class MainActivity : AppCompatActivity() {
         galeryBtn.setOnClickListener {
             val callGalleryIntent = Intent(Intent.ACTION_GET_CONTENT)
             callGalleryIntent.type = "image/*"
-            if(callGalleryIntent.resolveActivity(packageManager)!=null) {
-                    startActivityForResult(callGalleryIntent,REQUEST_IMAGE_CAPTURE)
-                }
+            if (callGalleryIntent.resolveActivity(packageManager) != null) {
+                startActivityForResult(callGalleryIntent, REQUEST_IMAGE_CAPTURE)
+            }
         }
-
         filtBtn.setOnClickListener {
 
-           randomMe()
+            randomMe()
         }
-
         imageBitmap = (photoImageView.drawable as BitmapDrawable).bitmap
         saveBtn.setOnClickListener {
-            val root = Environment.getExternalStorageDirectory().toString()
-            val myDir = File(root + "/capture_photo")
-            myDir.mkdirs()
-            val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-            val OutletFname = "Image-$timeStamp.jpg"
-            val file = File(myDir, OutletFname)
-            if (file.exists()) file.delete()
-            try {
-                val out = FileOutputStream(file)
-                imageBitmap = (photoImageView.drawable as BitmapDrawable).bitmap
-                imageBitmap?.compress(Bitmap.CompressFormat.JPEG, 100, out)
-                out.flush()
-                out.close()
-                Toast.makeText(this,"Save complete!",Toast.LENGTH_SHORT).show()
-            } catch (e: Exception) {
-                e.printStackTrace()
+            if (ActivityCompat.checkSelfPermission(applicationContext,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this@MainActivity,
+                        arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 102)
             }
+                val root = Environment.getExternalStorageDirectory().toString()
+                val myDir = File(root + "/capture_photo")
+                myDir.mkdirs()
+                val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+                val OutletFname = "Image-$timeStamp.jpg"
+                val file = File(myDir, OutletFname)
+                if (file.exists()) file.delete()
+                try {
+                    val out = FileOutputStream(file)
+                    imageBitmap = (photoImageView.drawable as BitmapDrawable).bitmap
+                    imageBitmap?.compress(Bitmap.CompressFormat.JPEG, 100, out)
+                    out.flush()
+                    out.close()
+                    Toast.makeText(this, "Save complete!", Toast.LENGTH_SHORT).show()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
         }
     }
      fun randomMe () {
