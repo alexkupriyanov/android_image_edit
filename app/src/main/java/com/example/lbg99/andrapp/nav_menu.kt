9275d4b5e2f0226eaf12a7219683d14948d9622e
Leonid WORK-PC <lbglbg9992gmail.com>
2018-05-05 12:21:01 +0700

    added navigation try to open diag

---
+++ app/src/main/java/com/example/lbg99/andrapp/nav_menu.kt
@@ -0,0 +1,111 @@
+package com.example.lbg99.andrapp
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_nav_menu.*
+import kotlinx.android.synthetic.main.app_bar_nav_menu.*
+import android.content.DialogInterface
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.PendingIntent.getActivity
+import android.R.string.cancel
+import android.app.DialogFragment
+
+
+
+
+
+class nav_menu : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_menu)
+        setSupportActionBar(toolbar)
+
+
+
+        fab.setOnClickListener {
+            val diag = Diag()
+            diag.show(fragmentManager, "000")
+        }
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+}
+
+class Diag : DialogFragment() {
+    val content = arrayOf("Capture", "Upload")
+    override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle("Upload")
+                .setItems(content, DialogInterface.OnClickListener { dialog, which ->
+                    // The 'which' argument contains the index position
+                    // of the selected item
+                })
+        return builder.create()
+    }
+}
