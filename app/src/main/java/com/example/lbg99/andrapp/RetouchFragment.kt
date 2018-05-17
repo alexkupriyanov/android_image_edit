8e5e528d6274ef25343ecef31cd4b1f14d79c9d3
Anya <annavladimostanina@gmail.com>
2018-05-17 17:20:43 +0700

    add

--- app/src/main/java/com/example/lbg99/andrapp/RetouchFragment.kt
+++ app/src/main/java/com/example/lbg99/andrapp/RetouchFragment.kt
@@ -12,6 +12,7 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.example.lbg99.andrapp.R.id.retouchView
 import kotlinx.android.synthetic.main.fragment_retouch.*
 import kotlinx.android.synthetic.main.nav_header_nav_menu.*
 
@@ -35,13 +36,9 @@ class RetouchFragment : Fragment() {
             override  fun  onTouch(v:View, event: MotionEvent):Boolean {
 
                 if (event.getAction() === MotionEvent.ACTION_MOVE){
-                    var x = event.x
-                    var y = event.y
-                    if (x > retouchView.width / 2 + tmpImage!!.width / 2 || x < retouchView.width / 2 - tmpImage!!.width / 2
-                    || y > retouchView.height / 2 + tmpImage!!.height / 2 || y < retouchView.height / 2 - tmpImage!!.height / 2) {
-                        Toast.makeText(context,"Вне области изображения", Toast.LENGTH_SHORT).show()
-                        return true
-                    }
+                    var x = event.x.toInt()
+                    var y = event.y.toInt()
+
                     val m_size = 2 * radius + 1
                     var x1 = 0
                     var y1 = 0
@@ -54,33 +51,36 @@ class RetouchFragment : Fragment() {
                     var sumG = 0
                     var sumB = 0
                     var count = 0
-                    val weights = Array<DoubleArray>(m_size, {DoubleArray(m_size)})
+                    val weights = Array(m_size, {DoubleArray(m_size)})
+                    if (x < 0 || y < 0 || x > width || y > height) return true
                     for (i in -radius until radius)// считаем коэффициент и количество пикселей в выбранном радиусе
                     {
                         for (j in -radius until radius)
                         {
+
                             val dX = i.toDouble()
                             val dY = j.toDouble()
                             val dR = radius.toDouble()
-                            weights[x1][y1] = (Math.pow(Math.E, -((dX * dX + dY * dY) / (2.0 * dR * dR)))) * 0.2
+                            weights[x1][y1] = (Math.pow(Math.E, -((dX * dX + dY * dY) / (2.0 * dR * dR)))) * 0.25
                             y1++
+
                         }
                         y1 = 0
                         x1++
                     }
-                    val s1= (x - radius).toInt() - retouchView.width / 2 + width / 2
-                    val f1=(x + radius).toInt() - retouchView.width / 2 + width / 2
-                    val s2= (y - radius).toInt() - retouchView.height / 2 + height / 2
-                    val f2=(y + radius).toInt() - retouchView.height / 2 + height / 2
+                    val s1= (x - radius).toInt()
+                    val f1=(x + radius).toInt()
+                    val s2= (y - radius).toInt()
+                    val f2=(y + radius).toInt()
                     for (i in s1 until f1) {
                         for (j in s2 until f2) {
                             var h=Math.sqrt(((x - i) * (x - i) + (y - j) * (y - j)).toDouble()).toInt()
-
-                                sumR += Color.red(pixels[j*width+i])
-                                sumG += Color.green(pixels[j*width+i])   // считаем сумму для цветов
-                                sumB += Color.blue(pixels[j*width+i])
-                                Log.d("RGB", "$i || $j")
+                            if (h <= radius && i > 0 && j > 0 && i < width && j < height) {
+                                sumR += Color.red(pixels[j * width + i])
+                                sumG += Color.green(pixels[j * width + i])   // считаем сумму для цветов
+                                sumB += Color.blue(pixels[j * width + i])
                                 count++
+                            }
                         }
                     }
                     sumR/=count
@@ -90,11 +90,17 @@ class RetouchFragment : Fragment() {
                     y1 = 0
                     for (i in s1 until f1) {
                         for (j in s2 until f2) {
-                                var  R = (sumR * weights[x1][y1]).toInt() + (Color.red(pixels[j * width + i]) * (1 - weights[x1][y1])).toInt()
-                                var  G = (sumG * weights[x1][y1]).toInt() + (Color.red(pixels[j * width + i]) * (1 - weights[x1][y1])).toInt()
-                                var  B = (sumB * weights[x1][y1]).toInt() + (Color.red(pixels[j * width + i]) * (1 - weights[x1][y1])).toInt()
+                            var h=Math.sqrt(((x - i) * (x - i) + (y - j) * (y - j)).toDouble()).toInt()
+                            if (h <= radius && i > 0 && j > 0 && i < width && j < height)
+                            {
+                                var  R = ((sumR * weights[x1][y1])) + (Color.red(pixels[j * width + i]) * (1 - weights[x1][y1]))
+                                var  G = (sumG * weights[x1][y1]) + (Color.red(pixels[j * width + i]) * (1 - weights[x1][y1]))
+                                var  B = (sumB * weights[x1][y1]) + (Color.red(pixels[j * width + i]) * (1 - weights[x1][y1]))
                                 y1++
-                                pixels[j * width + i] = 255 * 16777216 +R * 65536 + G * 256 + B
+                                val a = 255
+                                val p = a shl 24 or (R.toInt() shl 16) or (G.toInt() shl 8) or B.toInt()
+                                pixels[j * width + i] = p
+                            }
                         }
                         y1=0
                         x1++
