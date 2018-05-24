880759edfe89d307131993bfa974f110dde5cc98
Leonid WORK-PC <lbglbg9992gmail.com>
2018-05-24 16:34:25 +0700

    fix filters to getpixels

--- app/src/main/java/com/example/lbg99/andrapp/FilterFragment.kt
+++ app/src/main/java/com/example/lbg99/andrapp/FilterFragment.kt
@@ -18,8 +18,8 @@ class FilterFragment : Fragment() {
     val TAG = "fragmentFilters"
     var curPath: String? = null
     private var tmpImage: Bitmap? = null
-    private var oldPixels: Array<IntArray>? = null
-    private var pixels: Array<IntArray>? = null
+
+
     override fun onAttach(context: Context?) {
         super.onAttach(context)
     }
@@ -40,8 +40,11 @@ class FilterFragment : Fragment() {
         super.onStart()
         curPath = commonData.currentPhotoPath
         tmpImage = commonData.imageBitmap
+        var width = tmpImage!!.width
+        var height = tmpImage!!.height
         photoView.setImageBitmap(tmpImage)
-
+        var oldPixels: IntArray = getPixelsMatrix(tmpImage)
+        var pixels: IntArray = oldPixels
         cancelFilterBtn.setOnClickListener {
             pixels = oldPixels
             tmpImage = commonData.imageBitmap
@@ -62,26 +65,26 @@ class FilterFragment : Fragment() {
                 when (position) {
 
                     1 -> {
-                        tmpImage = bin(pixels)
-                        pixels = getPixelsMatrix(tmpImage)
+                        pixels = bin(pixels)
+                        tmpImage = Bitmap.createBitmap(pixels, width, height, Bitmap.Config.RGB_565)
                         photoView.setImageBitmap(tmpImage)
                     }
 
                     2 -> {
-                        tmpImage = inverse(pixels)
-                        pixels = getPixelsMatrix(tmpImage)
+                        pixels = inverse(pixels)
+                        tmpImage = Bitmap.createBitmap(pixels, width, height, Bitmap.Config.RGB_565)
                         photoView.setImageBitmap(tmpImage)
                     }
 
                     3 -> {
-                        tmpImage = gray(pixels)
-                        pixels = getPixelsMatrix(tmpImage)
+                        pixels= gray(pixels)
+                        tmpImage = Bitmap.createBitmap(pixels, width, height, Bitmap.Config.RGB_565)
                         photoView.setImageBitmap(tmpImage)
                     }
 
                     4 -> {
-                        tmpImage = sepia(pixels)
-                        pixels = getPixelsMatrix(tmpImage)
+                        pixels= sepia(pixels)
+                        tmpImage = Bitmap.createBitmap(pixels, width, height, Bitmap.Config.RGB_565)
                         photoView.setImageBitmap(tmpImage)
                     }
                 }
@@ -89,57 +92,48 @@ class FilterFragment : Fragment() {
         }
     }
 
-    fun getPixelsMatrix(tmpImage: Bitmap?): Array<IntArray>? { //получает матрицу пикселей из bitmap (просто интовые байты)
-        var arr = Array(tmpImage!!.width, { IntArray(tmpImage!!.height) })
-        for(i in 0 until tmpImage!!.width)
+    fun getPixelsMatrix(tmpImage: Bitmap?): IntArray { //получает матрицу пикселей из bitmap (просто интовые байты)
+        var arr = IntArray(tmpImage!!.width * tmpImage!!.height)
+        /*for(i in 0 until tmpImage!!.width)
             for(j in 0 until tmpImage!!.height) {
                 arr[i][j]= tmpImage!!.getPixel(i, j)
-            }
+            }*/
+        tmpImage!!.getPixels(arr, 0, tmpImage!!.width, 0, 0, tmpImage!!.width, tmpImage!!.height)
         return arr // закинули в глобальный массив
     }
 
-    fun bin(pxl : Array<IntArray>?) : Bitmap? {
+    fun bin(pxl : IntArray) : IntArray {
         var matrix = pxl
-        for (i in 0 until tmpImage!!.width)
-            for(j in 0 until tmpImage!!.height)
+        for (i in 0 until tmpImage!!.height)
+            for(j in 0 until tmpImage!!.width)
             {
-                val color = matrix!![i][j]
+                val color = matrix!![i * tmpImage!!.width + j]
                 val r = Color.red(color)
                 val g = Color.green(color)
                 val b = Color.blue(color)
                 val luminance = 0.299 * r + 0.587 * g + 0.114 * b
-                matrix[i][j] = if (luminance > 125) Color.WHITE else Color.BLACK
+                matrix[i * tmpImage!!.width + j] = if (luminance > 125) Color.WHITE else Color.BLACK
             }
-        var tmp: Bitmap? = Bitmap.createBitmap(tmpImage!!.width, tmpImage!!.height, Bitmap.Config.RGB_565)
-        for(i in 0 until matrix!!.size)
-            for(j in 0 until matrix[i].size)
-                tmp!!.setPixel(i, j, matrix[i][j])
-        pixels = matrix
-        return tmp
+        return matrix
     }
 
-    fun inverse(pxl : Array<IntArray>?) : Bitmap? {
+    fun inverse(pxl : IntArray) : IntArray {
         var matrix = pxl
-        for (i in 0 until tmpImage!!.width)
-            for(j in 0 until tmpImage!!.height)
+        for (i in 0 until tmpImage!!.height)
+            for(j in 0 until tmpImage!!.width)
             {
-                val color = matrix!![i][j]
+                val color = matrix!![i * tmpImage!!.width + j]
                 val r = 255 - Color.red(color)
                 val g = 255 - Color.green(color)
                 val b = 255 - Color.blue(color)
                 val a = 255
                 val p = a shl 24 or (r shl 16) or (g shl 8) or b
-                matrix[i][j] = p
+                matrix[i * tmpImage!!.width + j] = p
             }
-        var tmp: Bitmap? = Bitmap.createBitmap(tmpImage!!.width, tmpImage!!.height, Bitmap.Config.RGB_565)
-        for(i in 0 until matrix!!.size)
-            for(j in 0 until matrix[i].size)
-                tmp!!.setPixel(i, j, matrix[i][j])
-        pixels = matrix
-        return tmp
+        return matrix
     }
 
-    fun gray(pxl : Array<IntArray>?) : Bitmap? {
+    fun gray(pxl : IntArray) : IntArray {
         var r: Int
         var g: Int
         var b: Int
@@ -147,9 +141,9 @@ class FilterFragment : Fragment() {
         var I: Double
         var Q: Double
         var matrix = pxl
-        for (i in 0 until tmpImage!!.width)
-            for (j in 0 until tmpImage!!.height) {
-                val color = matrix!![i][j]
+        for (i in 0 until tmpImage!!.height)
+            for (j in 0 until tmpImage!!.width) {
+                val color = matrix!![i * tmpImage!!.width + j]
                 r = Color.red(color)
                 g = Color.green(color)
                 b = Color.blue(color)
@@ -166,17 +160,12 @@ class FilterFragment : Fragment() {
                 b = Math.max(0, Math.min(255, b))
                 val a = 255
                 val p = a shl 24 or (r shl 16) or (g shl 8) or b
-                matrix[i][j] = p
+                matrix[i * tmpImage!!.width + j] = p
             }
-        var tmp: Bitmap? = Bitmap.createBitmap(tmpImage!!.width, tmpImage!!.height, Bitmap.Config.RGB_565)
-        for(i in 0 until matrix!!.size)
-            for(j in 0 until matrix[i].size)
-                tmp!!.setPixel(i, j, matrix[i][j])
-        pixels = matrix
-        return tmp
+        return matrix
     }
 
-    fun sepia(pxl : Array<IntArray>?) : Bitmap? {
+    fun sepia(pxl : IntArray) : IntArray {
         var r: Int
         var g: Int
         var b: Int
@@ -184,10 +173,10 @@ class FilterFragment : Fragment() {
         var I: Double
         var Q: Double
         var matrix = pxl
-        for (i in 0 until tmpImage!!.width)
-            for(j in 0 until tmpImage!!.height)
+        for (i in 0 until tmpImage!!.height)
+            for(j in 0 until tmpImage!!.width)
             {
-                val color = matrix!![i][j]
+                val color = matrix!![i * tmpImage!!.width + j]
                 r = Color.red(color)
                 g = Color.green(color)
                 b = Color.blue(color)
@@ -204,14 +193,9 @@ class FilterFragment : Fragment() {
                 b = Math.max(0, Math.min(255, b))
                 val a = 255
                 val p = a shl 24 or (r shl 16) or (g shl 8) or b
-                matrix[i][j] = p
+                matrix[i * tmpImage!!.width + j] = p
             }
-        var tmp: Bitmap? = Bitmap.createBitmap(tmpImage!!.width, tmpImage!!.height, Bitmap.Config.RGB_565)
-        for(i in 0 until matrix!!.size)
-            for(j in 0 until matrix[i].size)
-                tmp!!.setPixel(i, j, matrix[i][j])
-        pixels = matrix
-        return tmp
+        return matrix
     }
 
     override fun onResume() {
