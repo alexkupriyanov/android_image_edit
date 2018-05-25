958a3319bbcdd586e31bb01156e5f50c33d9d58b
DESKTOP-LL83653\lbg99 <lbg9992@mail.ru>
2018-05-25 15:42:48 +0700

    fix rotation

--- app/src/main/java/com/example/lbg99/andrapp/TurnFragment.kt
+++ app/src/main/java/com/example/lbg99/andrapp/TurnFragment.kt
@@ -3,6 +3,7 @@ package com.example.lbg99.andrapp
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.graphics.Matrix
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -77,26 +78,49 @@ class TurnFragment : Fragment() {
 
     }
     fun Rotate(value: Double, bitmap: Bitmap?) : Bitmap {
-        var value = value
-        value = Math.toRadians(value)
+        var value = Math.toRadians(-value)
         //инициализация переменных перед преобазованием
         val w = bitmap!!.width
         val h = bitmap!!.height
         val pixels = IntArray(w * h)
         bitmap!!.getPixels(pixels, 0, w, 0, 0, w, h)
-        //поворот currentBitmap
-        val tmp = rotate(value, w, h, pixels)
-        return tmp
-    }
+        val newWidth = (h * Math.abs(Math.sin(value)) + w * Math.abs(Math.cos(value))).toInt()
+        val newHeight = (h * Math.abs(Math.cos(value)) + w * Math.abs(Math.sin(value))).toInt()
+        val newPixels = IntArray(newHeight * newWidth)
+        val oX = w / 2
+        val oY = h / 2 //центр исходной картинки
+        val dX = newWidth / 2 - oX //сдвиг центра
+        val dY = newHeight / 2 - oY //сдвиг центра
+        var pixelA: Int
+        var pixelB: Int
+        var pixelC: Int
+        var pixelD: Int
+        var x: Int
+        var y: Int
+        //вычисление новых пикселей
+        for (i in 0 until newHeight) {
+            for (j in 0 until newWidth) {
+                //исходные координаты
+                x = (oX + (j.toDouble() - oX - dX) * Math.cos(value) - (i.toDouble() - oY - dY) * Math.sin(value)).toInt()
+                y = (oY + (j.toDouble() - oX - dX) * Math.sin(value) + (i.toDouble() - oY - dY) * Math.cos(value)).toInt()
+                if (y > 0 && x > 0 && y < h - 2 && x < w - 2) {
+                    //усреднение пикселей
+                    pixelA = pixels[y * w + x]
+                    pixelB = pixels[y * w + (x + 1)]
+                    pixelC = pixels[(y + 1) * w + x]
+                    pixelD = pixels[(y + 1) * w + (x + 1)]
+                    val R = (Color.red(pixelA) + Color.red(pixelB) + Color.red(pixelC) + Color.red(pixelD)) / 4
+                    val G = (Color.green(pixelA) + Color.green(pixelB) + Color.green(pixelC) + Color.green(pixelD)) / 4
+                    val B = (Color.blue(pixelA) + Color.blue(pixelB) + Color.blue(pixelC) + Color.blue(pixelD)) / 4
+                    val Al = (Color.alpha(pixelA) + Color.alpha(pixelB) + Color.alpha(pixelC) + Color.alpha(pixelD)) / 4
 
-    /** повораичвает изображение на заданный угол  */
-    //сейчас использует встроенные средства; возможно, это надо исправить
-    fun rotate(alpha: Double, w: Int, h: Int, pixels: IntArray): Bitmap {
-        val matrix = Matrix()
-        matrix.postRotate(Math.toDegrees(alpha).toFloat())
-        var temp = Bitmap.createBitmap(pixels, w, h, Bitmap.Config.ARGB_8888)
-        temp = Bitmap.createBitmap(temp, 0, 0, w, h, matrix, true)
-        return temp
+                    newPixels[i * newWidth + j] = Al * 16777216 + R * 65536 + G * 256 + B
+                } else {
+                    newPixels[i * newWidth + j] = 0x00FFFFFF
+                }
+            }
+        }
+        return Bitmap.createBitmap(newPixels, newWidth, newHeight, Bitmap.Config.RGB_565)
     }
     override fun onStop() {
         super.onStop()
